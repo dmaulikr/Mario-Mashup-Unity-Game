9bcea3223fa2a9447d73ba3e9b268a9dc1fcfe03
Tobias <W1654477@losrios.edu>
2017-09-27 19:40:40 -0700

    Initial files for Mario mashup

---
+++ Assets/Scripts/Collision/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+	// keep track of game elements which can pick this up
+	public string targetTag = "Player";
+
+	void OnTriggerEnter2D(Collider2D target) {
+		if (target.gameObject.tag == targetTag) {
+			OnCollect (target.gameObject);
+			OnDestroy ();
+		}
+
+	}
+
+
+	// Use this for initialization
+	protected virtual void OnCollect (GameObject target) {
+	
+	}
+	
+	// Update is called once per frame
+	protected virtual void OnDestroy () {
+		Destroy (gameObject);
+	}
+}
